52683823a669daa383088403eb010bff9095adc2
barman47 <nomsouzoanya@yahoo.co.uk>
2018-11-25 11:21:18 +0100

    Initial Commit

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    var header = $("nav");
+    var links = $('.links');
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+            links.addClass('scrolled-links');
+        } else {
+            header.removeClass("scrolled");
+            links.removeClass('scrolled-links');
+        }
+    });
+});
